acff58a4199df43a1e0504da23906b6fde1388ba
yves-haswany <167018271+yves-haswany@users.noreply.github.com>
2024-12-09 13:43:40 +0200

    Update calculatebilling.js

--- netlify/functions/calculatebilling.js
+++ netlify/functions/calculatebilling.js
@@ -1,1 +1,6 @@
+const calculateBilling = (reads, cdr) => {
+  const totalKwh = reads.reduce((sum, read) => sum + read.kWh, 0);
+  const cost = totalKwh * cdr.rate;
+  return { totalKwh, cost };
+};
 
